6a38749aee20581a981184a8b2b1ab9d89d535c4
Yuri Fukuda <airinterface@gmail.com>
2022-11-08 15:07:55 -0500

    init draft

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # cornersign
-e-paper project
+
+E-paper project
+
+Dependency. 
+
+set "lib" dir in [https://github.com/waveshare/e-Paper/tree/master/RaspberryPi_JetsonNano/python](https://github.com/waveshare/e-Paper/tree/master/RaspberryPi_JetsonNano/python)
---
+++ app.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+import sys
+import os
+libdir = os.getenv('E_PAPER_LIB_DIR')
+picdir = os.path.dirname(os.path.realpath(__file__))
+if os.path.exists(libdir):
+    sys.path.append(libdir)
+
+import logging
+from waveshare_epd import epd7in5_V2
+import time
+from PIL import Image,ImageDraw,ImageFont
+import traceback
+
+logging.basicConfig(level=logging.DEBUG)
+
+try:
+    logging.info("epd7in5_V2 Demo")
+    epd = epd7in5_V2.EPD()
+    
+    logging.info("init and Clear")
+    epd.init()
+    epd.Clear()
+
+    font24 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 24)
+    font40 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 40)
+
+    # Drawing on the Horizontal image
+    logging.info("1.Drawing on the Horizontal image...")
+    Himage = Image.new('1', (epd.width, epd.height), 255)  # 255: clear the frame
+    draw = ImageDraw.Draw(Himage)
+    draw.text((10, 0), 'hello world', font = font40, fill = 0)
+    epd.display(epd.getbuffer(Himage))
+    time.sleep(2)
+
+    # logging.info("Clear...")
+    # epd.init()
+    # epd.Clear()
+
+    # logging.info("Goto Sleep...")
+    # epd.sleep()
+    
+except IOError as e:
+    logging.info(e)
+    
+except KeyboardInterrupt:    
+    logging.info("ctrl + c:")
+    epd7in5_V2.epdconfig.module_exit()
+    exit()
